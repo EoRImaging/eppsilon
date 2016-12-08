1bad38592b4261a2fd181a24f7f75fed2f8cb5d6
Nichole Barry <nbarry@uw.edu>
2016-12-07 19:17:45 -0500

    Revert grid engine code to normal until we update

--- ps_wrappers/PS_list_job.sh
+++ ps_wrappers/PS_list_job.sh
@@ -17,7 +17,6 @@ done < $obs_list_path
 #Make power spectra through a ps wrapper in idl
 
 input_file=${file_path_cubes}/
-filter_name='none'
-#delta_uv_lambda=2
-idl -IDL_DEVICE ps -IDL_CPU_TPOOL_NTHREADS $nslots -e mit_ps_job -args $input_file $version $filter_name #$delta_uv_lambda 
+
+idl -IDL_DEVICE ps -IDL_CPU_TPOOL_NTHREADS $nslots -e mit_ps_job -args $input_file $version
 
