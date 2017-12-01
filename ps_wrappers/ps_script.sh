6a03ebbab731e132ac179dbd67af937497d2f6b4
Nichole Barry <nbarry@uw.edu>
2017-12-01 00:53:43 -0500

    fixed chunk integration bug

--- ps_wrappers/ps_script.sh
+++ ps_wrappers/ps_script.sh
@@ -233,9 +233,9 @@ if [ "$ps_only" -ne "1" ]; then
 	    	    message=$(qsub ${hold_str} -l h_vmem=$mem,h_stack=512k,h_rt=$wallclock_time -V -v file_path_cubes=$FHDdir,obs_list_path=$chunk_obs_list,version=$version,chunk=$chunk,nslots=$nslots,legacy=$legacy,evenodd=$evenodd,pol=$pol -e $errfile -o $outfile -pe chost $nslots ${PSpath}ps_wrappers/integrate_job.sh)
 	    	    message=($message)
 	    	    if [ "$chunk" -eq 1 ] && [[ "$evenodd" = "even" ]] && [[ "$pol" = "XX" ]]; then idlist=${message[2]}; else idlist=${idlist},${message[2]}; fi
-	    	    echo Combined_obs_${version}_int_chunk${chunk} >> $sub_cubes_list # trick it into finding our sub cubes
 		done
 	    done
+	    echo Combined_obs_${version}_int_chunk${chunk} >> $sub_cubes_list # trick it into finding our sub cubes
         done
 
         # master integrator
